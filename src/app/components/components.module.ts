a0a563346d8656348f729b6e0dd20a048ee338b0
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-01-24 20:13:03 -0500

    create card

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CardComponent } from './card/card.ts';
+
+@NgModule({
+  declarations: [
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  exports: [
+
+  ]
+})
+export class AppModule { }
