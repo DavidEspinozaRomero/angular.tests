9a606ba80cdb981c8619a2080eac2d373721582b
David Espinoza <deerhou@gmail.com>
2022-01-26 14:00:11 -0500

    indtalando prime ng y creando estructura base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BtnComponent } from './btn/btn.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { InputNumberComponent } from './input-number/input-number.component';
+import { BtnSumComponent } from './btn-sum/btn-sum.component';
+import { ListaPersonasComponent } from './lista-personas/lista-personas.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BtnComponent,
+    CalculadoraComponent,
+    InputNumberComponent,
+    BtnSumComponent,
+    ListaPersonasComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
