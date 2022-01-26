89e6a211cce6515c0a382eb36d9d6b99061fc54f
David Espinoza <deerhou@gmail.com>
2022-01-26 14:05:39 -0500

    primer commit xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BtnComponent } from './btn/btn.component';
-import { CalculadoraComponent } from './calculadora/calculadora.component';
-import { InputNumberComponent } from './input-number/input-number.component';
-import { BtnSumComponent } from './btn-sum/btn-sum.component';
-import { ListaPersonasComponent } from './lista-personas/lista-personas.component';
-import { CarouselComponent } from './carousel/carousel.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BtnComponent,
-    CalculadoraComponent,
-    InputNumberComponent,
-    BtnSumComponent,
-    ListaPersonasComponent,
-    CarouselComponent
   ],
   imports: [
     BrowserModule,
