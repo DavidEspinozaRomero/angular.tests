922d405b1a398eb240aa461c0e18fc462d723911
David Espinoza <deerhou@gmail.com>
2022-01-26 14:11:40 -0500

    agregando contact, blog

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products/products.component';
 import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 
 @NgModule({
   declarations: [
     ProductsComponent,
-    MainComponent
+    MainComponent,
+    ContactComponent,
+    BlogComponent
   ],
   imports: [
     CommonModule
