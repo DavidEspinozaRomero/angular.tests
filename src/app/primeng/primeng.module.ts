07964952a5ee9860cc296e07533e1cc778d8c5ee
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-01-26 20:55:53 -0500

    menu bar

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 
-// import {MenubarModule} from 'primeng/menubar';
-// import {MenuItem} from 'primeng/api';
-import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
-import {MenuItem} from 'primeng/api';                  //api
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
 
 @NgModule({
   exports: [
