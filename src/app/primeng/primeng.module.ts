0a579d7aadf4d92637ccb803ba9f45425df5f6bb
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-01-26 21:04:32 -0500

    prime menu

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import {MenubarModule} from 'primeng/menubar';
-import {MenuItem} from 'primeng/api';
 
 @NgModule({
   exports: [
     MenubarModule,
-    MenuItem,
   ]
 })
 export class PrimengModule { }
