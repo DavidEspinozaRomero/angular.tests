57c2979c4c5568013d7b3b822398c2b15c84051c
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-01-26 21:09:50 -0500

    agregando menu demo

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -11,28 +11,28 @@ export class MenuComponent implements OnInit {
 
   items: MenuItem[] = [
     {
-        label: 'File',
-        items: [{
-                label: 'New', 
-                icon: 'pi pi-fw pi-plus',
-                items: [
-                    {label: 'Project'},
-                    {label: 'Other'},
-                ]
-            },
-            {label: 'Open'},
-            {label: 'Quit'}
-        ]
-    },
-    {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-            {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-            {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-        ]
-    }
-];
+      label: 'File',
+      items: [{
+        label: 'New', 
+        icon: 'pi pi-fw pi-plus',
+      items: [
+        {label: 'Project'},
+        {label: 'Other'},
+      ]
+      },
+      {label: 'Open'},
+      {label: 'Quit'}
+      ]
+      },
+      {
+      label: 'Edit',
+      icon: 'pi pi-fw pi-pencil',
+      items: [
+        {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+        {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+      ]
+      }
+    ];
 
   constructor() { }
 
