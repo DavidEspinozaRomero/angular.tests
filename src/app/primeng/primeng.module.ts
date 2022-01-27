93b456a826e332829f15cb906d87cfde4e6005ed
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-01-26 20:49:22 -0500

    pakage

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import {MenubarModule} from 'primeng/menubar';
-import {MenuItem} from 'primeng/api';
+// import {MenubarModule} from 'primeng/menubar';
+// import {MenuItem} from 'primeng/api';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';                  //api
 
 @NgModule({
   exports: [
