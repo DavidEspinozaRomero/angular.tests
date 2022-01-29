f87c28ce11d53df677c27743a362b8ef68b43a40
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-01-28 20:15:14 -0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
 import { PagesRoutingModule } from './pages/pages-routing.module';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
@@ -21,6 +22,7 @@ import { SharedModule } from './shared/shared.module';
     PagesRoutingModule,
     SharedModule,
     PagesModule,
+    ComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-import { ComponentsModule } from '../componentes/components.module';
+import { PrimengModule } from '../primeng/primeng.module';
 import { ProductsComponent } from './products/products.component';
 import { MainComponent } from './main/main.component';
 import { ContactComponent } from './contact/contact.component';
 import { BlogComponent } from './blog/blog.component';
 import { GalleryComponent } from './gallery/gallery.component';
-import { PrimengModule } from '../primeng/primeng.module';
 import { PagesRoutingModule } from './pages-routing.module';
+import { ComponentsModule } from '../components/components.module';
 
 
 
