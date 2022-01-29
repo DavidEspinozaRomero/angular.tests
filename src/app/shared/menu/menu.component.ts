2318012a6bc2552daac4804d5a3b75d07ea19a3a
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-01-28 20:38:36 -0500

    router link

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -12,30 +12,35 @@ export class MenuComponent implements OnInit {
   items: MenuItem[] = [
     {
       label: 'Products',
-      routerLink: 'products',
+      routerLink: 'productos',
       items: 
         [
           {
             label: 'Bordado a mano', 
             icon: 'pi pi-fw pi-plus',
+            routerLink: 'bordadoamano',
           },
           {
             label: 'Bordado a maquina', 
             icon: 'pi pi-fw pi-dollar',
+            routerLink: 'bordadoamaquina',
           },
           {
             label: 'Punto de cruz', 
             icon: 'pi pi-fw pi-dollar',
+            routerLink: 'puntodecruz',
           },
         ]
     },
     {
       label: 'Contacto',
       icon: 'pi pi-fw pi-pencil',
+      routerLink: 'contacto',
     },
     {
       label: 'Nosotros',
       icon: 'pi pi-fw pi-gear',
+      routerLink: 'nosotros',
     }
   ];
 
