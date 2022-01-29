593f122064bf64b6765477c0cdba51cdcca2c488
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-01-28 19:53:51 -0500

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
 // import { LayoutsComponent } from './layouts/layouts.component';
 // import { AuthGuard } from './core/guards/auth.guard';
 
-const routes: Routes = [] 
-// [
-//   { path: '', component: LayoutsComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),canActivate: [AuthGuard] },
+const routes: Routes = [
+  { path: '', component: AppComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
 //   { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
-// ];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
