9bdfbfd655e3f3c2edd0047c63f5712a3187622f
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-01-29 21:56:00 -0500

    test img

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  @Input() src?:string
+  @Input() src?:string = 'https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
   @Input() alt?:string
   @Input() title?:string
   @Input() description?:string
