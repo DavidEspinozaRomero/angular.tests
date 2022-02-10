1701fd85d5dea746ac7bbb4e50d6be685c3674b8
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-02-09 20:28:07 -0500

    rutas

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -4,12 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { ProductsComponent } from './products/products.component';
 import { MainComponent } from './main/main.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const routes: Routes = [
 	{ path: '', component: MainComponent },
 	{ path: 'productos', component: ProductsComponent },
 	{ path: 'contacto', component: ContactComponent },
-	// { path: 'nosotros', component: ProductsComponent },
+	{ path: 'nosotros', component: GalleryComponent },
 	{ path: '**', redirectTo: '' },
 ];
 
